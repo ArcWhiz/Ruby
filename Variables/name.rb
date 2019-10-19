1529c058d6407a3de6a70995fbee127ca5d836dc
Tushar Garg <37308109+ArcWhiz@users.noreply.github.com>
2019-10-19 23:07:51 +0530

    Update name.rb

--- Variables/name.rb
+++ Variables/name.rb
@@ -1,3 +1,7 @@
-puts "Enter your name"
-name = gets.chomp
+puts "Enter your first name"
+first_name = gets.chomp
+puts "Enter your last name"
+last_name = gets.chomp
+name = first_name + " " + last_name
 puts "Hello, #{name}!"
+10.times {puts name}
